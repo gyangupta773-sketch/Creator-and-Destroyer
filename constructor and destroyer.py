b09ad7c986e2b647c1b980e057d063130a738a3b
gyangupta773-sketch <gyangupta773@gmail.com>
2026-02-03 17:22:37 +0530

    Add files via upload

---
+++ constructor and destroyer.py
@@ -0,0 +1,11 @@
+class Employee:
+
+    def _init_(self):
+        print("Employee created")
+
+    def _del_(self):
+        print("Destructor called, Employee created")
+
+obj = Employee()
+
+del obj
